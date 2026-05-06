a6fe7d8bbdca1b0193fe12d09db46dce6eadd260
Satish <sathishchandas@gmail.com>
2026-05-06 00:07:32 -0400

    Very simple h1 tag with React.

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Greeting from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render( // This line finds the root element in the index.html file and creates a React root for rendering the application.
   <StrictMode>
     <App />
   </StrictMode>,
