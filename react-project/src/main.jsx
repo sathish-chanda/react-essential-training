588612a65fe56c011cfbc134c53f75730ede344a
Satish <sathishchandas@gmail.com>
2026-05-06 00:10:30 -0400

    Cleanup. Creating a react element git push

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -1,11 +1,6 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Greeting from './App.jsx'
 
-createRoot(document.getElementById('root')).render( // This line finds the root element in the index.html file and creates a React root for rendering the application.
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')).render(<App />);
