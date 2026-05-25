8cff300da7291ab2bb8141dd7ff702d41141c072
Satish <sathishchandas@gmail.com>
2026-05-25 10:40:41 -0400

    Created a new next react project. run npm create-next-app@latest. Choose the project name. Select TypeScript - No Select ESlint - No Select Tailwind - Yes Select src/ - Yes Select App Router - Yes Select Custom Default Imports - No

---
+++ nextjs-project/next-react-project/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <h1>More React to Come!</h1>
+    </main>
+  );
+}
