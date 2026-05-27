1be69860e19b4818f91a18ae90a12233a283e69a
Satish <sathishchandas@gmail.com>
2026-05-27 09:36:12 -0400

    Fetching Data in React Components and passing data as props.

--- nextjs-project/next-react-project/src/app/hotels/page.js
+++ nextjs-project/next-react-project/src/app/hotels/page.js
@@ -1,8 +1,25 @@
-export default function Page() {
+async function getData() { // This function may take the time to fetch the data, so we need to make it async
+  const res = await fetch( // Wait for the fetch to complete before moving on
+    "https://snowtooth-api-rest.fly.dev"
+  );
+  const data = await res.json();
+  return data;
+}
+function HotelBlock({name, capacity}) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{capacity}</p>
+    </div>
+  )
+}
+export default async function Page() {
+  const data = await getData(); // This will return a promise, not the actual data
   return (
     <main>
       <div>
         <h1>Hotel Details</h1>
+        <div>{data.map((hotel) => <HotelBlock key={hotel.id} name={hotel.name} capacity={hotel.capacity} />)}</div>
       </div>
     </main>
   );
--- nextjs-project/next-react-project/src/app/mountain/page.js
+++ nextjs-project/next-react-project/src/app/mountain/page.js
@@ -1,7 +1,34 @@
-export default function Page() {
+async function getData() { // This function may take the time to fetch the data, so we need to make it async
+  const res = await fetch( // Wait for the fetch to complete before moving on
+    "https://snowtooth-api-rest.fly.dev"
+  );
+  const data = await res.json();
+  return data;
+}
+
+export default async function Page() {
+  const data = await getData(); // This will return a promise, not the actual data
   return (
     <main>
       <h1>Lift Status Info</h1>
+      {/* <div>{JSON.stringify(data)}</div> */}
+      <table>
+        <thead>
+          <tr>
+            <th>Lift Name</th>
+            <th>Current Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((lift) => (
+            <tr key={lift.id}>
+              <td>{lift.name}</td>
+              <td>{lift.status}</td>
+            </tr>
+          ))} 
+          <tr></tr>
+        </tbody>
+      </table>
     </main>
   );
 }
