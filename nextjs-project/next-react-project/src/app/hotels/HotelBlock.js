0d04c5f2c297ff0fd27258d42c94a2e8d20cd2cc
Satish <sathishchandas@gmail.com>
2026-05-28 16:00:34 -0400

    Added the CLIENT COMPONENT and Image tag in the hotels.

---
+++ nextjs-project/next-react-project/src/app/hotels/HotelBlock.js
@@ -0,0 +1,17 @@
+"use client";
+import Image from "next/image";
+
+export default function HotelBlock({id, name, capacity}) {
+  
+  const imageLoader = ({ src }) => {
+    return `./hotels/${src}.jpeg`;
+  };
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{capacity}</p>
+      <Image src={id} alt={name} width={300} height={200} loader={imageLoader}/>
+    </div>
+  )
+}
--- nextjs-project/next-react-project/src/app/hotels/page.js
+++ nextjs-project/next-react-project/src/app/hotels/page.js
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import HotelBlock from "./HotelBlock";
+
 async function getData() { // This function may take the time to fetch the data, so we need to make it async
   const res = await fetch( // Wait for the fetch to complete before moving on
     "https://snowtooth-api-rest.fly.dev"
@@ -5,21 +8,18 @@ async function getData() { // This function may take the time to fetch the data,
   const data = await res.json();
   return data;
 }
-function HotelBlock({name, capacity}) {
-  return (
-    <div>
-      <h2>{name}</h2>
-      <p>{capacity}</p>
-    </div>
-  )
-}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+} 
+
 export default async function Page() {
   const data = await getData(); // This will return a promise, not the actual data
   return (
     <main>
       <div>
         <h1>Hotel Details</h1>
-        <div>{data.map((hotel) => <HotelBlock key={hotel.id} name={hotel.name} capacity={hotel.capacity} />)}</div>
+        <div>{data.map((hotel) => <HotelBlock key={hotel.id} id={`0${getRandomInt(1, 6)}`} name={hotel.name} capacity={hotel.capacity} />)}</div>
       </div>
     </main>
   );
