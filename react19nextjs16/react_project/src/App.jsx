d01a46c47971f2065b929030fb3cf82586f0fada
Satish <sathishchandas@gmail.com>
2026-06-04 08:20:29 -0400

    Writing markup in react

--- react19nextjs16/react_project/src/App.jsx
+++ react19nextjs16/react_project/src/App.jsx
@@ -1,5 +1,17 @@
 const App = () => {
-  return <h1>React is fun!!</h1>
+  return (
+    <>
+      <h1>React is fun!!</h1>
+      <p>React makes building UIs easy and efficient.</p>
+      <img src="images/nature.jpeg" width="300" alt="Nature Image" />
+      <input 
+        type="text"
+        placeholder="Type your name"
+        autoComplete="off"
+      />
+    </>
+  );
+
 }
 
 // Export the App component
