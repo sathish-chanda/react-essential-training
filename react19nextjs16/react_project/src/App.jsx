b54f99d63849ee00b24b69959dcac86c2b45c1e7
Satish <sathishchandas@gmail.com>
2026-06-04 23:03:35 -0400

    Integration Javascript into JSX using the curly braces.

--- react19nextjs16/react_project/src/App.jsx
+++ react19nextjs16/react_project/src/App.jsx
@@ -1,4 +1,17 @@
 const App = () => {
+  const userName = "Alice";
+
+  const greetUser = (name) => `Hello, ${name}! Welcome to React!`;
+
+  const userInfo = { age: 30, location: "New York" };
+
+  const titleStyle = {
+    color: "blue",
+    fontSize: "24px",
+    textAlign: "center",
+    margin: "20px 0"
+  };
+
   return (
     <>
       <h1>React is fun!!</h1>
@@ -9,6 +22,21 @@ const App = () => {
         placeholder="Type your name"
         autoComplete="off"
       />
+
+      <div>
+        {/* Passing string with quotes */}
+        <img src="images/nature.jpeg" width="300" alt="Nature Image" />
+
+        {/* Referencing a javascript variable */}
+        <p>Your name is: {userName}!</p>
+
+        <p>{greetUser(userName)}</p>
+
+        <p>Age: {userInfo.age}, Location: {userInfo.location}.</p>
+
+        <h1 style={titleStyle}>Welcome to React!</h1>
+
+      </div>
     </>
   );
 
