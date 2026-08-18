3dcac50711068adb60dee0d65131e78a9d8fbf3a
Satish <sathishchandas@gmail.com>
2026-08-17 23:35:06 -0400

    Conditional rendering the shopping cart based on the show cart use state.

--- projects/shopping-cart/src/App.jsx
+++ projects/shopping-cart/src/App.jsx
@@ -1,5 +1,20 @@
 import ShoppingCart from "./ShoppingCart"
+import {useState} from "react";
+const App = () => {
+  const [showCart,setShowCart] = useState(true)
 
-const App = () => <ShoppingCart /> 
+  const removeCart = () => {
+    setShowCart(prev => !prev)
+  }
+  return (
+    <>
+      <div>
+        <button onClick={removeCart}>{showCart ? 'Remove' : 'Show' } ShoppingCart</button>
+      </div>
+      {showCart && <ShoppingCart />}
+    </>
+  )
+
+}
 
 export default App
