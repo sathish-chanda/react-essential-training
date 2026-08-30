c1562f6f2ec1113523c7a30f40f2968494525e9d
Satish <sathishchandas@gmail.com>
2026-08-30 12:58:52 -0400

    Integrated media display component. That will enable to display images and videos. The videos will be autoplayed.

--- projects/uk-driving-test/src/components/MediaDisplay.jsx
+++ projects/uk-driving-test/src/components/MediaDisplay.jsx
@@ -1,7 +1,40 @@
-const MediaDisplay = () => {
+const MediaDisplay = ({ type, image, video, autoPlayVideo = true }) => {
   return (
-    <>
-    </>
+    <div style={{
+      flex: 1,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      {type === "image" && (
+        <img src={image}
+         alt="question"
+         style={{
+          maxWidth: "100%",
+          aspectRatio: "16 / 9",
+          objectFit: "cover",
+          borderRadius: "8px",
+         }} />
+      )}
+      {type === "video" && (
+        <video 
+          controls
+          autoPlay={autoPlayVideo}
+          muted={autoPlayVideo}
+          style={{
+            maxWidth: "100%",
+            aspectRatio: "16 / 9",
+            objectFit: "cover",
+            borderRadius: "8px",
+          }}>
+            <source 
+              src={video}
+              type="video/mp4"
+            />
+          </video>
+      )}
+
+    </div>
   )
 }
 
--- projects/uk-driving-test/src/components/QuestionView.jsx
+++ projects/uk-driving-test/src/components/QuestionView.jsx
@@ -2,6 +2,7 @@ import Card from "./ui/Card"
 import QuestionHeader from "./QuestionHeader";
 import QuestionText from "./QuestionText";
 import AnswerOptions from "./AnswerOptions";
+import MediaDisplay from "./MediaDisplay"
 const QuestionView = ( {
   q,
   currentQuestion,
@@ -19,12 +20,18 @@ const QuestionView = ( {
       <div style={{ display: "flex", gap: "1rem", flexDirection: "row"}}>
          <div style={{ flex: 1}}>
           <QuestionText text={q.question} />
+          <AnswerOptions options={q.options} 
+            selectedIndex={selectedAnswer}
+            onSelect={onAnswer} /> 
          </div>
+         {(q.type === "image" || q.type === "video") && (
+          <MediaDisplay 
+            type={q.type}
+            image={q.image}
+            video={q.video} 
+            />
+         )}
       </div>
-      <AnswerOptions options={q.options} 
-        selectedIndex={selectedAnswer}
-        onSelect={onAnswer}
-      />
     </Card>
   )
 }
